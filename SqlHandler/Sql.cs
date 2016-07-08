ef15fc5b0af046dfe921ea27d71dec72796cd429
SamuelWr <samuel.wraak@gmail.com>
2016-07-08 14:17:01 +0200

    Added prototypes for SqlHandler Functions

--- SqlHandler/Sql.cs
+++ SqlHandler/Sql.cs
@@ -9,5 +9,24 @@ namespace SqlHandler
     public static class Sql
     {
         private const string ConStr = "something here";
+
+        public static bool LogIn(string username, string password)
+        {
+            return false;
+        }
+        public static bool CreateUser(string userName, string Password, bool isAdmin)
+        {
+            bool success = false;
+
+            return success;
+        }
+        public static void GetProducts()
+        {
+            //return list of products;
+        }
+        public static void SearchProducts(string searchString)
+        {
+            // retrurn list of products
+        }
     }
 }
