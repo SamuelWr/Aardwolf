3eac3c5740071c983cae46ebc2a43d62ed93c790
SamuelWr <samuel.wraak@gmail.com>
2016-07-08 16:45:02 +0200

    Added some comments

--- SqlHandler/Sql.cs
+++ SqlHandler/Sql.cs
@@ -4,12 +4,20 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace SqlHandler
 {
     public static class Sql
     {
         private const string CON_STR = "Data Source=localhost;Initial Catalog=Aardwolf;Integrated Security=SSPI";
+        private static Random random = new Random();
+        private const int lengthOfSalt = 10;
+
+        /// <summary>
+        /// For testing only, liable to be removed without warning.
+        /// </summary>
+        /// <returns></returns>
         public static string GetUsers()
         {
             string users = "";
@@ -43,20 +51,60 @@ namespace SqlHandler
 
             return users;
         }
-        public static bool LogIn(string username, string password)
+        /// <summary>
+        /// Attempts to log in user, returns userId if successful else -1
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static int LogIn(string username, string password)
         {
-            return false;
+            int userId=-1;
+            //Todo: get password hash and salt from database
+
+            //Todo: hash from input password.
+
+            //Todo: compare and return
+            return userId;
         }
-        public static bool CreateUser(string userName, string Password, bool isAdmin)
+        /// <summary>
+        /// Creates a new user, returns userId if successful -1 else
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="Password"></param>
+        /// <param name="isAdmin"></param>
+        /// <returns></returns>
+        public static int CreateUser(string userName, string Password, bool isAdmin)
         {
+            int userId = -1;
+            //Todo: check that username does not already exist.
+
+            char[] salt = new char[lengthOfSalt];
+            for (int i = 0; i < lengthOfSalt; i++)
+            {
+                salt[i] = (char)random.Next(65, 91);
+            }
+            var md5 = new System.Security.Cryptography.MD5Cng();
+            //md5.ComputeHash(Password);
+            //md5.ComputeHash()
+
+
+
             bool success = false;
 
-            return success;
+            return userId;
         }
+        /// <summary>
+        /// returns list of all products
+        /// </summary>
         public static void GetProducts()
         {
             //return list of products;
         }
+        /// <summary>
+        /// returns list of all products matching search string
+        /// </summary>
+        /// <param name="searchString"></param>
         public static void SearchProducts(string searchString)
         {
             // retrurn list of products
